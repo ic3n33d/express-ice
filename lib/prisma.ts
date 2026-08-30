873afad04ebccd7964b4645f8a48969e6cc405d9
Ic3n33d <iceneed@outlook.com>
2026-08-30 11:48:22 +0100

    Remove unnecessary pg adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-// 1. Initialize the new Prisma 7 adapter with your database URL
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL! 
-});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// 2. Pass the adapter directly into the PrismaClient constructor
-// @ts-ignore
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
